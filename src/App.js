e42154fde0088588aa2437064be0cc70c5830156
Napat <s5935512003@phuket.psu.ac.th>
2020-04-28 02:24:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from '@reach/router'
+import About from './pages/About/About'
+import Contact from './pages/Contact.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Blog from './pages/Blog.jsx'
+import Home from './pages/Home.jsx'
+import ResponsiveNavigation from './components/ResponsiveNavigation'
+import logo from './Logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const navLinks = [
+		{
+			text: 'Home',
+			path: '/',
+			icon: 'ion-ios-home'
+		},
+		{
+			text: 'Marketplace',
+			path: '/Marketplace',
+			icon: 'ion-ios-cart'
+		},
+		{
+			text: 'Contact',
+			path: '/contact',
+			icon: 'ion-ios-megaphone'
+		},
+		{
+			text: 'About',
+			path: '/about',
+			icon: 'ion-ios-business'
+		},
+		{
+			text: 'Login',
+			path: '/Login',
+		},
+		
+	]
 
+	return (
+		<div className="App">
+			<ResponsiveNavigation
+				navLinks={ navLinks }
+				logo={ logo }
+				background="#95C4B1"
+				hoverBackground="#ddd"
+				linkColor="#fff"
+			/>
+			<Router>
+				<Contact path="/contact" />
+				<Home path="/home" />
+				<About path="/about" />
+			</Router>
+		</div>
+	);
+}
 export default App;
